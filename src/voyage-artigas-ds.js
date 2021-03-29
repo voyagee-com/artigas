ce28211bd62f3eade1eafc8b5420db5f2c234c60
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-03-29 09:48:33 -0300

    Parcial commit

--- src/voyage-artigas-ds.js
+++ src/voyage-artigas-ds.js
@@ -1,7 +1,4 @@
 import "./globals.css"
-// Anything exported from this file is importable by other in-browser modules.
-// export function publicApiFunction() {
-// }
 
 export { Button } from './components/Button/Button'
 export { DateTime } from './components/DateTime/DateTime'
